3480a484ae31b1d54ee213f5efa41ec4f4726969
nicolodarpa <100917955+nicolodarpa@users.noreply.github.com>
2022-04-04 21:54:54 +0200

    update code

--- src/main/java/it/polimi/ingsw/model/Tower.java
+++ src/main/java/it/polimi/ingsw/model/Tower.java
@@ -2,11 +2,11 @@ package it.polimi.ingsw.model;
 
 public class Tower {
     private int id;
-    private String color;
+    private TowerColor color;
     private boolean positionInGame;
 
 
-    public Tower(int id, String color) {
+    public Tower(int id, TowerColor color) {
         this.id = id;
         this.color = color;
         this.positionInGame = true;
